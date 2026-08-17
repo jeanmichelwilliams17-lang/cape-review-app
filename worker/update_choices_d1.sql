8e4131cf4b44716b69a6555ae2f4d6285eaadd5d
ralmeer <jeanmichelwilliams3@gmail.com>
2026-08-17 19:20:06 -0400

    fix: update unspaced square root choices in D1 choices table and add digit-command spacing regex to iOS stripLaTeXWrapper

---
+++ worker/update_choices_d1.sql
@@ -0,0 +1,93 @@
+UPDATE choices SET answer_raw = '3 \sqrt{5}', answer_code = '3 \sqrt{5}' WHERE answer_raw = '3\sqrt{5}' OR answer_code = '3\sqrt{5}';
+UPDATE choices SET answer_raw = '5 \sqrt{3}', answer_code = '5 \sqrt{3}' WHERE answer_raw = '5\sqrt{3}' OR answer_code = '5\sqrt{3}';
+UPDATE choices SET answer_raw = '9 \sqrt{5}', answer_code = '9 \sqrt{5}' WHERE answer_raw = '9\sqrt{5}' OR answer_code = '9\sqrt{5}';
+UPDATE choices SET answer_raw = '\frac{1}{9} \tan^{(-$2)} x + c', answer_code = '\frac{1}{9} \tan^{(-$2)} x + c' WHERE answer_raw = '\frac{1}{9} \tan^{-1} x + c' OR answer_code = '\frac{1}{9} \tan^{-1} x + c';
+UPDATE choices SET answer_raw = '\frac{1}{3} \tan^{(-$2)} 3x + c', answer_code = '\frac{1}{3} \tan^{(-$2)} 3x + c' WHERE answer_raw = '\frac{1}{3} \tan^{-1} 3x + c' OR answer_code = '\frac{1}{3} \tan^{-1} 3x + c';
+UPDATE choices SET answer_raw = '\frac{1}{3} \tan^{(-$2)} \frac{x}{3} + c', answer_code = '\frac{1}{3} \tan^{(-$2)} \frac{x}{3} + c' WHERE answer_raw = '\frac{1}{3} \tan^{-1} \frac{x}{3} + c' OR answer_code = '\frac{1}{3} \tan^{-1} \frac{x}{3} + c';
+UPDATE choices SET answer_raw = '\frac{1}{9} \tan^{(-$2)} 3x + c', answer_code = '\frac{1}{9} \tan^{(-$2)} 3x + c' WHERE answer_raw = '\frac{1}{9} \tan^{-1} 3x + c' OR answer_code = '\frac{1}{9} \tan^{-1} 3x + c';
+UPDATE choices SET answer_raw = '\frac{1}{2 \sqrt{2}}', answer_code = '\frac{1}{2 \sqrt{2}}' WHERE answer_raw = '\frac{1}{2\sqrt{2}}' OR answer_code = '\frac{1}{2\sqrt{2}}';
+UPDATE choices SET answer_raw = '2 \sqrt{2}', answer_code = '2 \sqrt{2}' WHERE answer_raw = '2\sqrt{2}' OR answer_code = '2\sqrt{2}';
+UPDATE choices SET answer_raw = '-\frac{3 \pi}{4}', answer_code = '-\frac{3 \pi}{4}' WHERE answer_raw = '-\frac{3\pi}{4}' OR answer_code = '-\frac{3\pi}{4}';
+UPDATE choices SET answer_raw = '\frac{3 \pi}{4}', answer_code = '\frac{3 \pi}{4}' WHERE answer_raw = '\frac{3\pi}{4}' OR answer_code = '\frac{3\pi}{4}';
+UPDATE choices SET answer_raw = '2e^{i\frac{2 \pi}{3}}', answer_code = '2e^{i\frac{2 \pi}{3}}' WHERE answer_raw = '2e^{i\frac{2\pi}{3}}' OR answer_code = '2e^{i\frac{2\pi}{3}}';
+UPDATE choices SET answer_raw = '2e^{i\frac{5 \pi}{6}}', answer_code = '2e^{i\frac{5 \pi}{6}}' WHERE answer_raw = '2e^{i\frac{5\pi}{6}}' OR answer_code = '2e^{i\frac{5\pi}{6}}';
+UPDATE choices SET answer_raw = '-2 \sqrt{4-x^2}', answer_code = '-2 \sqrt{4-x^2}' WHERE answer_raw = '-2\sqrt{4-x^2}' OR answer_code = '-2\sqrt{4-x^2}';
+UPDATE choices SET answer_raw = '4 \frac{1}{2}\%', answer_code = '4 \frac{1}{2}\%' WHERE answer_raw = '4\frac{1}{2}\%' OR answer_code = '4\frac{1}{2}\%';
+UPDATE choices SET answer_raw = '6 \frac{1}{4}\%', answer_code = '6 \frac{1}{4}\%' WHERE answer_raw = '6\frac{1}{4}\%' OR answer_code = '6\frac{1}{4}\%';
+UPDATE choices SET answer_raw = '2 \Phi(2) - 1', answer_code = '2 \Phi(2) - 1' WHERE answer_raw = '2\Phi(2) - 1' OR answer_code = '2\Phi(2) - 1';
+UPDATE choices SET answer_raw = '2 \sqrt{m}E', answer_code = '2 \sqrt{m}E' WHERE answer_raw = '2\sqrt{m}E' OR answer_code = '2\sqrt{m}E';
+UPDATE choices SET answer_raw = '5 \pi\text{ cm}', answer_code = '5 \pi\text{ cm}' WHERE answer_raw = '5\pi\text{ cm}' OR answer_code = '5\pi\text{ cm}';
+UPDATE choices SET answer_raw = '\frac{7 \pi}{2}\text{ radians}', answer_code = '\frac{7 \pi}{2}\text{ radians}' WHERE answer_raw = '\frac{7\pi}{2}\text{ radians}' OR answer_code = '\frac{7\pi}{2}\text{ radians}';
+UPDATE choices SET answer_raw = '\frac{3 \pi}{4}\text{ radians}', answer_code = '\frac{3 \pi}{4}\text{ radians}' WHERE answer_raw = '\frac{3\pi}{4}\text{ radians}' OR answer_code = '\frac{3\pi}{4}\text{ radians}';
+UPDATE choices SET answer_raw = '\frac{3 \pi}{2}\text{ radians}', answer_code = '\frac{3 \pi}{2}\text{ radians}' WHERE answer_raw = '\frac{3\pi}{2}\text{ radians}' OR answer_code = '\frac{3\pi}{2}\text{ radians}';
+UPDATE choices SET answer_raw = '3 \pi\text{ radians}', answer_code = '3 \pi\text{ radians}' WHERE answer_raw = '3\pi\text{ radians}' OR answer_code = '3\pi\text{ radians}';
+UPDATE choices SET answer_raw = '144 \pi \times 10^{-5} \text{ T}', answer_code = '144 \pi \times 10^{-5} \text{ T}' WHERE answer_raw = '144\pi \times 10^{-5} \text{ T}' OR answer_code = '144\pi \times 10^{-5} \text{ T}';
+UPDATE choices SET answer_raw = '1.44 \pi \times 10^{-5} \text{ T}', answer_code = '1.44 \pi \times 10^{-5} \text{ T}' WHERE answer_raw = '1.44\pi \times 10^{-5} \text{ T}' OR answer_code = '1.44\pi \times 10^{-5} \text{ T}';
+UPDATE choices SET answer_raw = '144 \pi \text{ } \mu\text{T}', answer_code = '144 \pi \text{ } \mu\text{T}' WHERE answer_raw = '144\pi \text{ } \mu\text{T}' OR answer_code = '144\pi \text{ } \mu\text{T}';
+UPDATE choices SET answer_raw = '144 \pi \text{ T}', answer_code = '144 \pi \text{ T}' WHERE answer_raw = '144\pi \text{ T}' OR answer_code = '144\pi \text{ T}';
+UPDATE choices SET answer_raw = '2 \sigma', answer_code = '2 \sigma' WHERE answer_raw = '2\sigma' OR answer_code = '2\sigma';
+UPDATE choices SET answer_raw = '4 \sigma', answer_code = '4 \sigma' WHERE answer_raw = '4\sigma' OR answer_code = '4\sigma';
+UPDATE choices SET answer_raw = '7 \sqrt{5}', answer_code = '7 \sqrt{5}' WHERE answer_raw = '7\sqrt{5}' OR answer_code = '7\sqrt{5}';
+UPDATE choices SET answer_raw = '13 \sqrt{5}', answer_code = '13 \sqrt{5}' WHERE answer_raw = '13\sqrt{5}' OR answer_code = '13\sqrt{5}';
+UPDATE choices SET answer_raw = '\frac{1 - 2 \sin^2 x}{\sec^2 x}', answer_code = '\frac{1 - 2 \sin^2 x}{\sec^2 x}' WHERE answer_raw = '\frac{1 - 2\sin^2 x}{\sec^2 x}' OR answer_code = '\frac{1 - 2\sin^2 x}{\sec^2 x}';
+UPDATE choices SET answer_raw = '\theta = \begin{cases} 2n\pi + \frac{\pi}{6} \ (2n + 1)\frac{5 \pi}{16} \end{cases}', answer_code = '\theta = \begin{cases} 2n\pi + \frac{\pi}{6} \ (2n + 1)\frac{5 \pi}{16} \end{cases}' WHERE answer_raw = '\theta = \begin{cases} 2n\pi + \frac{\pi}{6} \ (2n + 1)\frac{5\pi}{16} \end{cases}' OR answer_code = '\theta = \begin{cases} 2n\pi + \frac{\pi}{6} \ (2n + 1)\frac{5\pi}{16} \end{cases}';
+UPDATE choices SET answer_raw = '\theta = \begin{cases} n\pi + \frac{\pi}{12} \ n\pi + \frac{5 \pi}{12} \end{cases}', answer_code = '\theta = \begin{cases} n\pi + \frac{\pi}{12} \ n\pi + \frac{5 \pi}{12} \end{cases}' WHERE answer_raw = '\theta = \begin{cases} n\pi + \frac{\pi}{12} \ n\pi + \frac{5\pi}{12} \end{cases}' OR answer_code = '\theta = \begin{cases} n\pi + \frac{\pi}{12} \ n\pi + \frac{5\pi}{12} \end{cases}';
+UPDATE choices SET answer_raw = '\theta = \begin{cases} n\pi + \frac{\pi}{12} \ (2n\pi) \frac{5 \pi}{12} \end{cases}', answer_code = '\theta = \begin{cases} n\pi + \frac{\pi}{12} \ (2n\pi) \frac{5 \pi}{12} \end{cases}' WHERE answer_raw = '\theta = \begin{cases} n\pi + \frac{\pi}{12} \ (2n\pi) \frac{5\pi}{12} \end{cases}' OR answer_code = '\theta = \begin{cases} n\pi + \frac{\pi}{12} \ (2n\pi) \frac{5\pi}{12} \end{cases}';
+UPDATE choices SET answer_raw = '\theta = \begin{cases} n\pi + \frac{\pi}{6} \ (n+1) \frac{5 \pi}{6} \end{cases}', answer_code = '\theta = \begin{cases} n\pi + \frac{\pi}{6} \ (n+1) \frac{5 \pi}{6} \end{cases}' WHERE answer_raw = '\theta = \begin{cases} n\pi + \frac{\pi}{6} \ (n+1) \frac{5\pi}{6} \end{cases}' OR answer_code = '\theta = \begin{cases} n\pi + \frac{\pi}{6} \ (n+1) \frac{5\pi}{6} \end{cases}';
+UPDATE choices SET answer_raw = '5 \sqrt{2}', answer_code = '5 \sqrt{2}' WHERE answer_raw = '5\sqrt{2}' OR answer_code = '5\sqrt{2}';
+UPDATE choices SET answer_raw = '3 \sqrt{2}', answer_code = '3 \sqrt{2}' WHERE answer_raw = '3\sqrt{2}' OR answer_code = '3\sqrt{2}';
+UPDATE choices SET answer_raw = '4 \sqrt{5}(\mathbf{i} - 2\mathbf{j})', answer_code = '4 \sqrt{5}(\mathbf{i} - 2\mathbf{j})' WHERE answer_raw = '4\sqrt{5}(\mathbf{i} - 2\mathbf{j})' OR answer_code = '4\sqrt{5}(\mathbf{i} - 2\mathbf{j})';
+UPDATE choices SET answer_raw = '48 \sqrt{y}', answer_code = '48 \sqrt{y}' WHERE answer_raw = '48\sqrt{y}' OR answer_code = '48\sqrt{y}';
+UPDATE choices SET answer_raw = '24 \sqrt{y}', answer_code = '24 \sqrt{y}' WHERE answer_raw = '24\sqrt{y}' OR answer_code = '24\sqrt{y}';
+UPDATE choices SET answer_raw = '16 \sqrt{y}', answer_code = '16 \sqrt{y}' WHERE answer_raw = '16\sqrt{y}' OR answer_code = '16\sqrt{y}';
+UPDATE choices SET answer_raw = '8 \sqrt{y}', answer_code = '8 \sqrt{y}' WHERE answer_raw = '8\sqrt{y}' OR answer_code = '8\sqrt{y}';
+UPDATE choices SET answer_raw = '8 \pi', answer_code = '8 \pi' WHERE answer_raw = '8\pi' OR answer_code = '8\pi';
+UPDATE choices SET answer_raw = '16 \pi', answer_code = '16 \pi' WHERE answer_raw = '16\pi' OR answer_code = '16\pi';
+UPDATE choices SET answer_raw = '32 \pi', answer_code = '32 \pi' WHERE answer_raw = '32\pi' OR answer_code = '32\pi';
+UPDATE choices SET answer_raw = '64 \pi', answer_code = '64 \pi' WHERE answer_raw = '64\pi' OR answer_code = '64\pi';
+UPDATE choices SET answer_raw = '\frac{x+y-2 \sqrt{xy}}{x-y}', answer_code = '\frac{x+y-2 \sqrt{xy}}{x-y}' WHERE answer_raw = '\frac{x+y-2\sqrt{xy}}{x-y}' OR answer_code = '\frac{x+y-2\sqrt{xy}}{x-y}';
+UPDATE choices SET answer_raw = '\frac{x-y+2 \sqrt{xy}}{x+y}', answer_code = '\frac{x-y+2 \sqrt{xy}}{x+y}' WHERE answer_raw = '\frac{x-y+2\sqrt{xy}}{x+y}' OR answer_code = '\frac{x-y+2\sqrt{xy}}{x+y}';
+UPDATE choices SET answer_raw = 'n\pi \pm \frac{2 \pi}{3}', answer_code = 'n\pi \pm \frac{2 \pi}{3}' WHERE answer_raw = 'n\pi \pm \frac{2\pi}{3}' OR answer_code = 'n\pi \pm \frac{2\pi}{3}';
+UPDATE choices SET answer_raw = '200 + \frac{5 \pi}{2}', answer_code = '200 + \frac{5 \pi}{2}' WHERE answer_raw = '200 + \frac{5\pi}{2}' OR answer_code = '200 + \frac{5\pi}{2}';
+UPDATE choices SET answer_raw = '200 + 5 \pi', answer_code = '200 + 5 \pi' WHERE answer_raw = '200 + 5\pi' OR answer_code = '200 + 5\pi';
+UPDATE choices SET answer_raw = '-3 \alpha, 4 \alpha', answer_code = '-3 \alpha, 4 \alpha' WHERE answer_raw = '-3\alpha, 4\alpha' OR answer_code = '-3\alpha, 4\alpha';
+UPDATE choices SET answer_raw = '\alpha, 4 \alpha', answer_code = '\alpha, 4 \alpha' WHERE answer_raw = '\alpha, 4\alpha' OR answer_code = '\alpha, 4\alpha';
+UPDATE choices SET answer_raw = '2 \sin 2 \theta \cos \theta', answer_code = '2 \sin 2 \theta \cos \theta' WHERE answer_raw = '2 \sin 2\theta \cos \theta' OR answer_code = '2 \sin 2\theta \cos \theta';
+UPDATE choices SET answer_raw = '2 \sin \theta \cos 2 \theta', answer_code = '2 \sin \theta \cos 2 \theta' WHERE answer_raw = '2 \sin \theta \cos 2\theta' OR answer_code = '2 \sin \theta \cos 2\theta';
+UPDATE choices SET answer_raw = '-2 \sin \theta \cos 2 \theta', answer_code = '-2 \sin \theta \cos 2 \theta' WHERE answer_raw = '-2 \sin \theta \cos 2\theta' OR answer_code = '-2 \sin \theta \cos 2\theta';
+UPDATE choices SET answer_raw = '-2 \sin 2 \theta \cos \theta', answer_code = '-2 \sin 2 \theta \cos \theta' WHERE answer_raw = '-2 \sin 2\theta \cos \theta' OR answer_code = '-2 \sin 2\theta \cos \theta';
+UPDATE choices SET answer_raw = '2 \pi', answer_code = '2 \pi' WHERE answer_raw = '2\pi' OR answer_code = '2\pi';
+UPDATE choices SET answer_raw = '4 \pi', answer_code = '4 \pi' WHERE answer_raw = '4\pi' OR answer_code = '4\pi';
+UPDATE choices SET answer_raw = '\frac{x + y - 2 \sqrt{xy}}{x - y}', answer_code = '\frac{x + y - 2 \sqrt{xy}}{x - y}' WHERE answer_raw = '\frac{x + y - 2\sqrt{xy}}{x - y}' OR answer_code = '\frac{x + y - 2\sqrt{xy}}{x - y}';
+UPDATE choices SET answer_raw = '\frac{x - y + 2 \sqrt{xy}}{x + y}', answer_code = '\frac{x - y + 2 \sqrt{xy}}{x + y}' WHERE answer_raw = '\frac{x - y + 2\sqrt{xy}}{x + y}' OR answer_code = '\frac{x - y + 2\sqrt{xy}}{x + y}';
+UPDATE choices SET answer_raw = '5 \sqrt{x}', answer_code = '5 \sqrt{x}' WHERE answer_raw = '5\sqrt{x}' OR answer_code = '5\sqrt{x}';
+UPDATE choices SET answer_raw = '4 \sqrt{x} + 1', answer_code = '4 \sqrt{x} + 1' WHERE answer_raw = '4\sqrt{x} + 1' OR answer_code = '4\sqrt{x} + 1';
+UPDATE choices SET answer_raw = '4 \sqrt{x} + \frac{\sqrt{x}}{4}', answer_code = '4 \sqrt{x} + \frac{\sqrt{x}}{4}' WHERE answer_raw = '4\sqrt{x} + \frac{\sqrt{x}}{4}' OR answer_code = '4\sqrt{x} + \frac{\sqrt{x}}{4}';
+UPDATE choices SET answer_raw = 'x = -3 + 4 \sin t 
+  y = 4 - 3 \cos t', answer_code = 'x = -3 + 4 \sin t 
+  y = 4 - 3 \cos t' WHERE answer_raw = 'x = -3 + 4\sin t 
+  y = 4 - 3\cos t' OR answer_code = 'x = -3 + 4\sin t 
+  y = 4 - 3\cos t';
+UPDATE choices SET answer_raw = 'x = 4 - 3 \sin t 
+  y = -4 - 3 \cos t', answer_code = 'x = 4 - 3 \sin t 
+  y = -4 - 3 \cos t' WHERE answer_raw = 'x = 4 - 3\sin t 
+  y = -4 - 3\cos t' OR answer_code = 'x = 4 - 3\sin t 
+  y = -4 - 3\cos t';
+UPDATE choices SET answer_raw = 'x = 3 + 4 \sin t 
+  y = -4 + 3 \cos t', answer_code = 'x = 3 + 4 \sin t 
+  y = -4 + 3 \cos t' WHERE answer_raw = 'x = 3 + 4\sin t 
+  y = -4 + 3\cos t' OR answer_code = 'x = 3 + 4\sin t 
+  y = -4 + 3\cos t';
+UPDATE choices SET answer_raw = 'x = -3 - 4 \cos t 
+  y = -4 + 3 \sin t', answer_code = 'x = -3 - 4 \cos t 
+  y = -4 + 3 \sin t' WHERE answer_raw = 'x = -3 - 4\cos t 
+  y = -4 + 3\sin t' OR answer_code = 'x = -3 - 4\cos t 
+  y = -4 + 3\sin t';
+UPDATE choices SET answer_raw = '2 \sin 2 \theta \cos \theta', answer_code = '2 \sin 2 \theta \cos \theta' WHERE answer_raw = '2\sin 2\theta \cos \theta' OR answer_code = '2\sin 2\theta \cos \theta';
+UPDATE choices SET answer_raw = '2 \sin \theta \cos 2 \theta', answer_code = '2 \sin \theta \cos 2 \theta' WHERE answer_raw = '2\sin \theta \cos 2\theta' OR answer_code = '2\sin \theta \cos 2\theta';
+UPDATE choices SET answer_raw = '-2 \sin 0 \cos 2 \theta', answer_code = '-2 \sin 0 \cos 2 \theta' WHERE answer_raw = '-2\sin 0 \cos 2\theta' OR answer_code = '-2\sin 0 \cos 2\theta';
+UPDATE choices SET answer_raw = '-2 \sin 2 \theta \cos 0', answer_code = '-2 \sin 2 \theta \cos 0' WHERE answer_raw = '-2\sin 2\theta \cos 0' OR answer_code = '-2\sin 2\theta \cos 0';
+UPDATE choices SET answer_raw = '9x^2 - 20 \sin 5x', answer_code = '9x^2 - 20 \sin 5x' WHERE answer_raw = '9x^2 - 20\sin 5x' OR answer_code = '9x^2 - 20\sin 5x';
+UPDATE choices SET answer_raw = '9x^2 + 20 \sin 5x', answer_code = '9x^2 + 20 \sin 5x' WHERE answer_raw = '9x^2 + 20\sin 5x' OR answer_code = '9x^2 + 20\sin 5x';
+UPDATE choices SET answer_raw = '18x - 100 \cos 5x', answer_code = '18x - 100 \cos 5x' WHERE answer_raw = '18x - 100\cos 5x' OR answer_code = '18x - 100\cos 5x';
+UPDATE choices SET answer_raw = '18x + 100 \cos 5x', answer_code = '18x + 100 \cos 5x' WHERE answer_raw = '18x + 100\cos 5x' OR answer_code = '18x + 100\cos 5x';
